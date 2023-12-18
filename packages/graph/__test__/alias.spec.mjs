a4ff354e5cdbdc677949b45147764bfa1d661a12
sun0day <sun_day500@163.com>
2023-12-18 22:37:53 +0800

    test: flaky alias test

--- packages/graph/__test__/alias.spec.mjs
+++ packages/graph/__test__/alias.spec.mjs
@@ -3,9 +3,15 @@ import {createParser, readParsedFile} from './utils.mjs'
 
 
 test('should parse alias path', async (t) => {
-  const parser = createParser({'@nested': './nested'})
+  const parser = createParser({'@nested': 'nested'})
 
   await parser.visit('./alias.js')
-
-  t.deepEqual(parser.parse(), readParsedFile('./alias.json'))
+  const parsed = parser.parse()
+  const expected = readParsedFile('./alias.json')
+  const foo = 'nested/foo.js'
+  t.truthy(parsed[foo].importer.includes('nested/bar.js'))
+  t.truthy(parsed[foo].importer.includes('alias.js'))
+  delete parsed[foo].importer
+  delete expected[foo].importer
+  t.deepEqual(parsed, expected) 
 })
